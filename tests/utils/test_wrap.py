05f4955720f0889d3eb08861493620a83b282ec0
Kush Dubey <kushdubey63@gmail.com>
2023-03-19 04:32:04 -0700

    Fix test_wrap

--- tests/utils/test_wrap.py
+++ tests/utils/test_wrap.py
@@ -16,33 +16,46 @@ def test_wraps_but_keep_wrapper_return_ann():
             return bool(func(*args, **kwargs))
         return wrapper
 
-    def sum(x: int, y: float, z) -> int:
+    def sum_numbers(x: int, y: float, z) -> int:
         return x + y + z
 
+    name_to_func = dict()
+    name_to_func['source'] = sum_numbers
+    ## Need to do this jankyness b/c I want to test that the wrapped function's
+    ## name is preserved. Without these references, sum_numbers can only refer
+    ## to the wrapped function below. 
+
     @boolify
-    def sum_wrapped(x: int, y: float, z) -> int:
+    def sum_numbers(x: int, y: float, z) -> int:
         return x + y + z
+
+    name_to_func['wrapped'] = sum_numbers
+
+    ## sanity check that the referenced functions are what we want
+    assert name_to_func['source'](1, 2, 3) == 6
+    assert name_to_func['wrapped'](1, 2, 3) is True
 
     ## check that the only modification is that __annotations__['return']
     ## comes from the wrapper
     for attr in WRAPPER_ASSIGNMENTS:
         if attr == '__annotations__':
-            annotations: dict = getattr(sum, attr)
-            annotations_wrapped: dict = getattr(sum_wrapped, attr)
-            assert annotations.keys() == annotations_wrapped.keys()
-            for key in annotations:
-                if key == 'return': ## then value should be bool, not int
-                    expected_type = 'bool'
+            annotations_source: dict = getattr(name_to_func['source'], attr)
+            annotations_wrapped: dict = getattr(name_to_func['wrapped'], attr)
+            assert annotations_source.keys() == annotations_wrapped.keys()
+            for key in annotations_source:
+                if key == 'return':
+                    expected_type = 'bool' ## not int!
                     observed_type = annotations_wrapped[key]
                     if not isinstance(observed_type, str):
                         ## didn't do from __future__ import annotations
                         observed_type = observed_type.__name__
                     assert observed_type == expected_type
                 else:
-                    assert annotations[key] == annotations_wrapped[key]
+                    assert annotations_source[key] == annotations_wrapped[key]
         else:
-            assert getattr(sum, attr) == getattr(sum, attr)
+            assert (getattr(name_to_func['source'], attr) ==
+                    getattr(name_to_func['wrapped'], attr))
 
     ## check that output is what we expect
-    assert sum_wrapped(-1, 2, 3) is True
-    assert sum_wrapped(-1, 1, 0) is False
+    assert name_to_func['wrapped'](-1, 2, 3) is True
+    assert name_to_func['wrapped'](-1, 1, 0) is False
